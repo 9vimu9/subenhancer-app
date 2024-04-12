7df331fb60a8c4888051149a45ade9715eeb4f1a
wimukthi <9wimu9@gmail.com>
2024-04-12 07:04:52 +0100

    Create  Vocabularies table.

---
+++ app/Enums/VocabularyEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums;
+
+enum VocabularyEnum: int
+{
+    case FILTERED_OUT_AS_KNOWN_USING_THE_TEST = 1;
+    case MARKED_AS_KNOWN = 2;
+    case MARKED_AS_UNKNOWN = 3;
+    case HAVE_NOT_SPECIFIED = 4;
+}
---
+++ database/migrations/2024_04_12_054211_create_vocabularies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Enums\VocabularyEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vocabularies', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('definition_id');
+            $table->foreign('definition_id')->references('id')->on('definitions')->onDelete('cascade');
+            $table->enum('vocabulary_type', [
+                VocabularyEnum::FILTERED_OUT_AS_KNOWN_USING_THE_TEST->name,
+                VocabularyEnum::MARKED_AS_KNOWN->name,
+                VocabularyEnum::MARKED_AS_UNKNOWN->name,
+                VocabularyEnum::HAVE_NOT_SPECIFIED->name,
+            ])->default(VocabularyEnum::HAVE_NOT_SPECIFIED->name);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vocabularies');
+    }
+};
