a326569a777480951d079cb267c51c6869e9f4f4
wimukthi <9wimu9@gmail.com>
2024-04-12 13:47:33 +0100

    moved dashboard components to seperate directory

--- resources/views/components/dashboard/layout.blade.php
+++ resources/views/components/dashboard/layout.blade.php
@@ -11,13 +11,13 @@
 
 <body>
 <div class="flex gap-8 bg-white dark:bg-gray-900">
-    <x-sidebar class="min-w-fit flex-grow-0 flex-shrink-0 hidden md:block"/>
+    <x-dashboard.sidebar class="min-w-fit flex-grow-0 flex-shrink-0 hidden md:block"/>
     <main class="mt-4 px-4">
         <div class="block sm:absolute top-5 right-8 order-1">
-            <x-dark-mode-toggle size="4" />
+            <x-dashboard.dark-mode-toggle size="4" />
         </div>
         {{ $slot }}
-        <x-footer />
+        <x-dashboard.footer />
     </main>
 </div>
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,24 @@
-<x-layout>
-    <x-header>
+<x-dashboard.layout>
+<x-dashboard.header>
         <x-slot:title>Dashboard</x-slot:title>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia sunt, assumenda dignissimos doloremque
         reiciendis autem iusto saepe ut minima nesciunt?
-    </x-header>
+    </x-dashboard.header>
 
     <section class="mt-8 flex flex-col md:flex-row gap-8">
-        <x-card>
+        <x-dashboard.card>
             <x-slot:title>2021 Stats</x-slot:title>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet, necessitatibus!
-        </x-card>
-        <x-card>
+        </x-dashboard.card>
+        <x-dashboard.card>
             <x-slot:title>2022 Stats</x-slot:title>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, fugit.
-        </x-card>
+        </x-dashboard.card>
     </section>
 
     <section class="mt-12">
-        <x-table />
+        <x-dashboard.table />
     </section>
 
-</x-layout>
+
+</x-dashboard.layout>
