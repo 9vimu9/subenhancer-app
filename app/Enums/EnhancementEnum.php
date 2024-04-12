fc1cd2c34bf56645b9009832c7ae40e145dbfa5d
wimukthi <9wimu9@gmail.com>
2024-04-12 09:04:34 +0100

    Create  Enhancements table.

---
+++ app/Enums/EnhancementEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum EnhancementEnum: int
+{
+    case PROCESSING = 1;
+    case FINISHED = 2;
+    case ADDED = 3;
+
+}
---
+++ database/migrations/2024_04_12_060945_create_enhancements_table.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Enums\EnhancementEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enhancements', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('source_id');
+            $table->foreign('source_id')->references('id')->on('sources')->onDelete('cascade');
+            $table->enum('status', [EnhancementEnum::PROCESSING->name, EnhancementEnum::FINISHED->name, EnhancementEnum::ADDED->name])->default(EnhancementEnum::ADDED->name);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enhancements');
+    }
+};
