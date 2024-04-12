cbd3e362ba8368ae6299dd692433814730ddb4d9
wimukthi <9wimu9@gmail.com>
2024-04-12 05:59:03 +0100

    Create dictionaries table.

---
+++ database/migrations/2024_04_12_044255_create_dictionaries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dictionaries', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('language_id');
+            $table->foreign('language_id')->references('id')->on('languages')->onDelete('cascade');
+            $table->unsignedBigInteger('definition_id');
+            $table->foreign('definition_id')->references('id')->on('definitions')->onDelete('cascade');
+            $table->string('translated_definition')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dictionaries');
+    }
+};
