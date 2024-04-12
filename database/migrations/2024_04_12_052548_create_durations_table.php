12e1252859385db11359495e4adfbcf446a1c621
wimukthi <9wimu9@gmail.com>
2024-04-12 06:36:36 +0100

    Create  Durations table.

---
+++ database/migrations/2024_04_12_052548_create_durations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('durations', function (Blueprint $table) {
+            $table->id();
+            $table->integer('start_time_in_millis')->nullable();
+            $table->integer('end_time_in_millis')->nullable();
+            $table->unsignedBigInteger('source_id');
+            $table->foreign('source_id')->references('id')->on('sources')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('durations');
+    }
+};
