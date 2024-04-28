bb50fe5a69e64daedb6aaf5a19b4481782547feb
wimukthi <9wimu9@gmail.com>
2024-04-29 00:26:41 +0100

    Modified default test case

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace Tests\Browser;
 
+use App\Traits\Testing\DatabaseMigrationsAndSeeders;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
 class ExampleTest extends DuskTestCase
 {
+    use DatabaseMigrationsAndSeeders;
+
     /**
      * A basic browser test example.
      */
