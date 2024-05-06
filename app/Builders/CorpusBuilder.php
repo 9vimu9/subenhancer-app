36702ec372abeff0b41a20795201f0adc672e68c
wimukthi <9wimu9@gmail.com>
2024-05-06 20:06:07 +0100

    added functions to CorpusBuilder.php

--- app/Builders/CorpusBuilder.php
+++ app/Builders/CorpusBuilder.php
@@ -5,11 +5,22 @@ declare(strict_types=1);
 namespace App\Builders;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 
 class CorpusBuilder extends Builder
 {
     public function hasWord(string $word): bool
     {
         return $this->where('word', $word)->exists();
+    }
+
+    public function findByWord(string $word): Model
+    {
+        return $this->where('word', $word)->firstOrFail();
+    }
+
+    public function saveWord(string $word): Model
+    {
+        return $this->create(['word' => $word]);
     }
 }
