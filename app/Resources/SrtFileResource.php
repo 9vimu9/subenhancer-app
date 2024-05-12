feb9e9a5ce01e12eba4af8c737a2bb96f1e5cf35
wimukthi <9wimu9@gmail.com>
2024-05-12 18:03:37 +0100

    Refactored the FileResource

--- app/Resources/SrtFileResource.php
+++ app/Resources/SrtFileResource.php
@@ -4,66 +4,22 @@ declare(strict_types=1);
 
 namespace App\Resources;
 
-use App\Models\Source;
-use App\Models\Srt;
-use App\Services\Captions\Caption;
+use App\Models\ResourceModels\ResourceModelInterface;
 use App\Services\Captions\CaptionsCollection;
-use Benlipp\SrtParser\Parser;
+use App\Services\SrtParser\SrtParserInterface;
 use Illuminate\Http\UploadedFile;
 
-class SrtFileResource implements FileResourceInterface, ResourceInterface
+class SrtFileResource extends AbstractFileResource
 {
-    public function __construct(private UploadedFile $file)
-    {
+    public function __construct(
+        protected UploadedFile $file,
+        protected ResourceModelInterface $resourceModel,
+        protected SrtParserInterface $srtParser,
+    ) {
     }
 
-    public function isAlreadyExist(): bool
+    public function toCaptions(): CaptionsCollection
     {
-        return Srt::query()->where('md5_hash', md5_file($this->file->getRealPath()))->exists();
-    }
-
-    public function fetch(): string
-    {
-        //        todo  : relocateFile method call must be removed somewhere else
-        $this->relocateFile();
-
-        return $this->file->getContent();
-    }
-
-    public function toCaptions(string $captionsString): CaptionsCollection
-    {
-        $captionsCollection = new CaptionsCollection();
-        $parser = new Parser();
-        $parser->loadString($captionsString);
-        foreach ($parser->parse() as $srt) {
-            $caption = new Caption();
-            $caption->setCaption($srt->text);
-            $startTime = (int) round($srt->startTime * 1000);
-            $endTime = (int) round($srt->endTime * 1000);
-            $caption->setStartTime($startTime);
-            $caption->setEndTime($endTime);
-            $captionsCollection->addCaption($caption);
-        }
-
-        return $captionsCollection;
-    }
-
-    public function storeResourceTable(): Source
-    {
-        $srt = Srt::query()->create(['file_location' => $this->file->getRealPath(), 'md5_hash' => md5_file($this->file->getRealPath())]);
-
-        return $srt->source()->create([]);
-    }
-
-    public function getFile(): UploadedFile
-    {
-        return $this->file;
-    }
-
-    public function relocateFile(): void
-    {
-        $hashedFileName = $this->file->hashName();
-        $movedFile = $this->file->move(storage_path('app'), $hashedFileName);
-        $this->file = new UploadedFile($movedFile->getRealPath(), $hashedFileName);
+        return $this->srtParser->parse($this->file->getContent());
     }
 }
