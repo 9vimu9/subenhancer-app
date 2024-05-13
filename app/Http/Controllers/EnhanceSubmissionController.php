4e4b33ee393e37bec676cd9559c8c9ef5342f32a
wimukthi <9wimu9@gmail.com>
2024-05-13 12:01:37 +0100

    added WordFilterApi to the EnhanceSubmissionController.php

--- app/Http/Controllers/EnhanceSubmissionController.php
+++ app/Http/Controllers/EnhanceSubmissionController.php
@@ -10,6 +10,7 @@ use App\Services\DefinitionsAPI\FreeDictionaryApi;
 use App\Services\EnhancementService;
 use App\Services\SentencesApi\FirstPartySentencingApi;
 use App\Services\SrtParser\BenlippStrParser;
+use App\Services\WordsFilterApi\FirstPartyWordFilterApi;
 use App\Services\YoutubeCaptionsGrabberApi\FirstPartyYoutubeCaptionsGrabberApi;
 use Illuminate\Http\RedirectResponse;
 
@@ -21,13 +22,14 @@ class EnhanceSubmissionController extends Controller
         $sentencingApi = new FirstPartySentencingApi();
         $srtParser = new BenlippStrParser();
         $youtubeCaptionsGrabberApi = new FirstPartyYoutubeCaptionsGrabberApi();
+        $wordFilterApi = new FirstPartyWordFilterApi();
         $resource = (new ResourceFactory())->generate(
             $request->file('subtitle_file'),
             $request->get('video_url'),
             $srtParser,
             $youtubeCaptionsGrabberApi,
         );
-        $enhancementService->submitEnhancement($resource, $definitionApi, $sentencingApi);
+        $enhancementService->submitEnhancement($resource, $definitionApi, $sentencingApi, $wordFilterApi);
 
         //        if ($request->hasFile('subtitle_file')) {
         //            return redirect()->back()->with(['toast' => ['type' => 'success', 'message' => 'File has been added successfully for the enhancement. You will receive a notification shortly']]);
