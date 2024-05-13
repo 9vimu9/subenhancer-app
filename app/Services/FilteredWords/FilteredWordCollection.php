ffc9af06d83754f664ebe2435334661d28d60cab
wimukthi <9wimu9@gmail.com>
2024-05-13 17:48:09 +0100

    Refactored the FilteredWordCollection.php

--- app/Services/FilteredWords/FilteredWordCollection.php
+++ app/Services/FilteredWords/FilteredWordCollection.php
@@ -4,10 +4,6 @@ declare(strict_types=1);
 
 namespace App\Services\FilteredWords;
 
-use App\Exceptions\CantFindDefinitionException;
-use App\Exceptions\DefinitionAlreadyExistException;
-use App\Services\DefinitionsAPI\DefinitionsApiInterface;
-
 class FilteredWordCollection
 {
     /**
@@ -30,23 +26,6 @@ class FilteredWordCollection
     public function toArray(): array
     {
         return $this->filteredWords;
-    }
-
-    public function storeNewFilteredWordsDefinitions(DefinitionsApiInterface $definitionsApi): void
-    {
-        foreach ($this->filteredWords as $index => $filteredWord) {
-            try {
-                $filteredWord->storeDefinitions($definitionsApi);
-            } catch (DefinitionAlreadyExistException $exception) {
-                continue;
-
-            } catch (CantFindDefinitionException $exception) {
-                unset($this->filteredWords[$index]);
-
-                continue;
-            }
-        }
-
     }
 
     public function toArrayOfWords(): array
