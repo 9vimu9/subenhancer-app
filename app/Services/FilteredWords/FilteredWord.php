12ddd408a22c4da381c5779727b596f6d1207201
wimukthi <9wimu9@gmail.com>
2024-05-13 17:41:36 +0100

    Refactored FilteredWord.php and FilteredWordCollection.php

--- app/Services/FilteredWords/FilteredWord.php
+++ app/Services/FilteredWords/FilteredWord.php
@@ -4,43 +4,33 @@ declare(strict_types=1);
 
 namespace App\Services\FilteredWords;
 
-use App\Exceptions\DefinitionAlreadyExistException;
-use App\Models\Corpus;
+use App\Exceptions\DefinitionsHasNotBeenDefinedException;
 use App\Services\Definitions\DefinitionCollection;
-use App\Services\DefinitionsAPI\DefinitionsApiInterface;
 
 class FilteredWord
 {
+    private DefinitionCollection $definitions;
+
     public function __construct(private string $word)
     {
-    }
-
-    public function isNewWord(): bool
-    {
-        return ! Corpus::query()->hasWord($this->word);
-
-    }
-
-    public function getDefinitions(DefinitionsApiInterface $definitionsApi): DefinitionCollection
-    {
-        return $definitionsApi->getDefinitions($this->word);
-    }
-
-    public function storeDefinitions(DefinitionsApiInterface $definitionsApi): void
-    {
-        if (! $this->isNewWord()) {
-            throw new DefinitionAlreadyExistException();
-        }
-        $definitions = $this->getDefinitions($definitionsApi)->toArray();
-        $corpus = Corpus::query()->saveWord($this->word);
-        foreach ($definitions as $definition) {
-            $definition->store((int) $corpus->id);
-        }
-
     }
 
     public function getWord(): string
     {
         return $this->word;
+    }
+
+    public function getDefinitions(): DefinitionCollection
+    {
+        if (! isset($this->definitions)) {
+            throw new DefinitionsHasNotBeenDefinedException();
+        }
+
+        return $this->definitions;
+    }
+
+    public function setDefinitions(DefinitionCollection $definitions): void
+    {
+        $this->definitions = $definitions;
     }
 }
--- tests/Feature/FilteredWordCollectionTest.php
+++ tests/Feature/FilteredWordCollectionTest.php
@@ -4,65 +4,46 @@ declare(strict_types=1);
 
 namespace Tests\Feature;
 
-use App\Enums\WordClassEnum;
-use App\Models\Corpus;
 use App\Services\FilteredWords\FilteredWord;
 use App\Services\FilteredWords\FilteredWordCollection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\Mocks\MockDefinitionsApi;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Tests\TestCase;
 
 class FilteredWordCollectionTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_to_array_method(): void
+    #[DataProvider('provideInputs')]
+    public function test_to_array_method(
+        FilteredWordCollection $filteredWordCollection,
+        FilteredWord $filteredWordOne,
+        FilteredWord $filteredWordTwo,
+    ): void {
+        $this->assertEqualsCanonicalizing([$filteredWordOne, $filteredWordTwo], $filteredWordCollection->toArray());
+    }
+
+    #[DataProvider('provideInputs')]
+    public function test_to_array_of_words(
+        FilteredWordCollection $filteredWordCollection,
+        FilteredWord $filteredWordOne,
+        FilteredWord $filteredWordTwo,
+    ): void {
+        $this->assertEquals(
+            [$filteredWordOne->getWord(), $filteredWordTwo->getWord()],
+            $filteredWordCollection->toArrayOfWords()
+        );
+
+    }
+
+    public static function provideInputs(): array
     {
         $filteredWordOne = new FilteredWord('filtered word 1');
         $filteredWordTwo = new FilteredWord('filtered word 2');
-        $expected = [$filteredWordOne, $filteredWordTwo];
         $filteredWordCollection = new FilteredWordCollection();
         $filteredWordCollection->addFilteredWord($filteredWordOne);
         $filteredWordCollection->addFilteredWord($filteredWordTwo);
-        $this->assertEqualsCanonicalizing($expected, $filteredWordCollection->toArray());
-    }
 
-    public function test_storeNewFilteredWordsDefinitions(): void
-    {
-        $word = 'Hello';
-        $filteredWord = new FilteredWord($word);
-        $collection = new FilteredWordCollection();
-        $collection->addFilteredWord($filteredWord);
-        $collection->storeNewFilteredWordsDefinitions(new MockDefinitionsApi());
-        $this->assertDatabaseHas('corpuses', ['word' => $word]);
-        $this->assertDatabaseHas('definitions', [
-            'definition' => '"Hello!" or an equivalent greeting.',
-            'word_class' => WordClassEnum::NOUN->name,
-        ]);
-
-    }
-
-    public function test_ignore_storing_definitions_for_exiting_words(): void
-    {
-        $word = 'Hello';
-        Corpus::factory()->create(['word' => $word]);
-        $filteredWord = new FilteredWord($word);
-        $collection = new FilteredWordCollection();
-        $collection->addFilteredWord($filteredWord);
-        $collection->storeNewFilteredWordsDefinitions(new MockDefinitionsApi());
-        $this->assertDatabaseEmpty('definitions');
-
-    }
-
-    public function test_remove_filtered_word_that_no_definition_available()
-    {
-        $filteredWordOne = new FilteredWord('Hello');
-        $filteredWordTwo = new FilteredWord('NO_DEFINITION_AVAILABLE');
-        $collection = new FilteredWordCollection();
-        $collection->addFilteredWord($filteredWordOne);
-        $collection->addFilteredWord($filteredWordTwo);
-        $collection->storeNewFilteredWordsDefinitions(new MockDefinitionsApi());
-        $this->assertEqualsCanonicalizing([$filteredWordOne], $collection->toArray());
-
+        return [[$filteredWordCollection, $filteredWordOne, $filteredWordTwo]];
     }
 }
