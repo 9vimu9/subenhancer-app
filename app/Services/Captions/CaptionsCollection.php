d6eda7316c3d7d2fd3ddc4f73585a188731c608c
wimukthi <9wimu9@gmail.com>
2024-05-13 11:46:20 +0100

    removed word filter responsibility from the CaptionsCollection.php

--- app/Services/Captions/CaptionsCollection.php
+++ app/Services/Captions/CaptionsCollection.php
@@ -4,12 +4,6 @@ declare(strict_types=1);
 
 namespace App\Services\Captions;
 
-use App\Exceptions\CaptionWordFilterException;
-use App\Services\FilteredWords\FilteredWord;
-use App\Services\FilteredWords\FilteredWordCollection;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Http;
-
 class CaptionsCollection
 {
     /**
@@ -32,24 +26,6 @@ class CaptionsCollection
     public function captions(): array
     {
         return $this->captions;
-    }
-
-    //list of unique words that is included in captions.
-    // This list should not be included duplicate words, stop words, nouns like place names
-    public function getFilteredWords(): FilteredWordCollection
-    {
-        $url = config('app.nlp_endpoint').'filter';
-        $response = Http::post($url, ['caption_string' => $this->toString()]);
-        if ($response->status() !== Response::HTTP_OK) {
-            throw new CaptionWordFilterException();
-        }
-        $filteredWords = $response->json()['filtered_words'];
-        $filteredWordsCollection = new FilteredWordCollection();
-        foreach ($filteredWords as $filteredWord) {
-            $filteredWordsCollection->addFilteredWord(new FilteredWord($filteredWord));
-        }
-
-        return $filteredWordsCollection;
     }
 
     public function toString(): string
