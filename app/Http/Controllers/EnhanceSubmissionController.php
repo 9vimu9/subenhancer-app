ef73a9f279e20ad594caad9c8f2fac752aaa0024
wimukthi <9wimu9@gmail.com>
2024-05-15 20:28:08 +0100

    WIP EnhanceSubmissionController.php

--- app/Http/Controllers/EnhanceSubmissionController.php
+++ app/Http/Controllers/EnhanceSubmissionController.php
@@ -6,30 +6,38 @@ namespace App\Http\Controllers;
 
 use App\Factories\ResourceFactory;
 use App\Http\Requests\SubmitEnhanceRequest;
-use App\Services\DefinitionsAPI\FreeDictionaryApi;
-use App\Services\EnhancementService;
-use App\Services\SentencesApi\FirstPartySentencingApi;
-use App\Services\SrtParser\BenlippStrParser;
-use App\Services\WordsFilterApi\FirstPartyWordFilterApi;
-use App\Services\YoutubeCaptionsGrabberApi\FirstPartyYoutubeCaptionsGrabberApi;
+use App\Services\CaptionSetviceInterface;
+use App\Services\DefinitionsServiceInterface;
+use App\Services\EnhancementServiceInterface;
+use App\Services\SrtParser\SrtParserInterface;
+use App\Services\WordServiceInterface;
+use App\Services\YoutubeCaptionsGrabberApi\YoutubeCaptionsGrabberApiInterface;
 use Illuminate\Http\RedirectResponse;
 
 class EnhanceSubmissionController extends Controller
 {
-    public function submit(SubmitEnhanceRequest $request, EnhancementService $enhancementService): RedirectResponse
-    {
-        $definitionApi = new FreeDictionaryApi();
-        $sentencingApi = new FirstPartySentencingApi();
-        $srtParser = new BenlippStrParser();
-        $youtubeCaptionsGrabberApi = new FirstPartyYoutubeCaptionsGrabberApi();
-        $wordFilterApi = new FirstPartyWordFilterApi();
+    public function submit(
+        SubmitEnhanceRequest $request,
+        EnhancementServiceInterface $enhancementService,
+        SrtParserInterface $srtParser,
+        YoutubeCaptionsGrabberApiInterface $youtubeCaptionsGrabberApi,
+        DefinitionsServiceInterface $definitionsService,
+        WordServiceInterface $wordService,
+        CaptionSetviceInterface $captionService
+    ): RedirectResponse {
         $resource = (new ResourceFactory())->generate(
             $request->file('subtitle_file'),
             $request->get('video_url'),
             $srtParser,
             $youtubeCaptionsGrabberApi,
         );
-        $enhancementService->submitEnhancement($resource, $definitionApi, $sentencingApi, $wordFilterApi);
+        $enhancementService->submitEnhancement(
+            $resource,
+            $definitionsService,
+            $wordService,
+            $captionService,
+
+        );
 
         //        if ($request->hasFile('subtitle_file')) {
         //            return redirect()->back()->with(['toast' => ['type' => 'success', 'message' => 'File has been added successfully for the enhancement. You will receive a notification shortly']]);
