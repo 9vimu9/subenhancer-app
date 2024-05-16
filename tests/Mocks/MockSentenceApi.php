067f1669fa9582cf0a86a667de2364dcb43217b4
wimukthi <9wimu9@gmail.com>
2024-05-16 01:08:15 +0100

    Created MockSentenceApi.php for mocking

---
+++ tests/Mocks/MockSentenceApi.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Mocks;
+
+use App\Services\Sentences\Sentence;
+use App\Services\Sentences\SentenceCollection;
+use App\Services\SentencesApi\SentencesApiInterface;
+
+class MockSentenceApi implements SentencesApiInterface
+{
+    public const string CAPTION_ONE = 'one';
+
+    public const string CAPTION_TWO = 'two';
+
+    public function getSentences(string $caption): SentenceCollection
+    {
+        $sentences = new SentenceCollection();
+        $sentenceOne = new Sentence();
+        $sentenceOne->setSentence(self::CAPTION_ONE);
+        $sentenceTwo = new Sentence();
+        $sentenceTwo->setSentence(self::CAPTION_TWO);
+        $sentences->add($sentenceOne);
+        $sentences->add($sentenceTwo);
+
+        return $sentences;
+    }
+}
