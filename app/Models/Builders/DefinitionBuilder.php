12226b479597ad2c65c34f55227634f69a1d9324
wimukthi <9wimu9@gmail.com>
2024-05-19 00:47:44 +0100

    Removed unused method

--- app/Models/Builders/DefinitionBuilder.php
+++ app/Models/Builders/DefinitionBuilder.php
@@ -5,17 +5,11 @@ declare(strict_types=1);
 namespace App\Models\Builders;
 
 use App\DataObjects\Definitions\Definition;
-use App\Models\Corpus;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class DefinitionBuilder extends Builder
 {
-    public function findDefinitionsByWord(string $word)
-    {
-        return Corpus::query()->findByWordOrFail($word)->definitions;
-    }
-
     public function createByDefinition(int $corpusId, Definition $definition): Model
     {
         return $this->create([
