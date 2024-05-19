54e7d2a710e7af51ba757dc89b98965d9a62780a
wimukthi <9wimu9@gmail.com>
2024-05-20 00:41:12 +0100

    Added new Builder

--- app/Models/Captionword.php
+++ app/Models/Captionword.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Models\Builders\FilteredwordBuilder;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,4 +14,9 @@ class Captionword extends Model
     protected $fillable = ['order_in_sentence', 'sentence_id', 'corpus_id', 'definition_id'];
 
     use HasFactory;
+
+    public function newEloquentBuilder($query): Builder
+    {
+        return new FilteredwordBuilder($query);
+    }
 }
