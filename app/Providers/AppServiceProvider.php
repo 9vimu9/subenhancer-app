b5f53ad6ef24a094a698a9e485ecf383bfcbe8c9
wimukthi <9wimu9@gmail.com>
2024-05-20 16:49:11 +0100

    Added events, interfaces to the AppServiceProvider.php to bind

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,26 +5,32 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use App\Apis\DefinitionsAPI\FreeDictionaryApi;
+use App\Apis\DefinitionSelectorApi\FirstPartyDefinitionSelectorApi;
 use App\Apis\SentencesApi\FirstPartySentencingApi;
 use App\Apis\SrtParser\BenlippStrParser;
 use App\Apis\WordsFilterApi\FirstPartyWordFilterApi;
 use App\Apis\YoutubeCaptionsGrabberApi\FirstPartyYoutubeCaptionsGrabberApi;
 use App\Core\Contracts\Apis\DefinitionsApiInterface;
+use App\Core\Contracts\Apis\DefinitionSelectorApiInterface;
 use App\Core\Contracts\Apis\SentencesApiInterface;
 use App\Core\Contracts\Apis\SrtParserInterface;
 use App\Core\Contracts\Apis\WordFilterApiInterface;
 use App\Core\Contracts\Apis\YoutubeCaptionsGrabberApiInterface;
 use App\Core\Contracts\Services\CaptionServiceInterface;
+use App\Core\Contracts\Services\DefinitionSelectorServiceInterface;
 use App\Core\Contracts\Services\DefinitionsServiceInterface;
 use App\Core\Contracts\Services\EnhancementServiceInterface;
 use App\Core\Contracts\Services\FilteredWordServiceInterface;
 use App\Core\Contracts\Services\SentenceServiceInterface;
 use App\Core\Contracts\Services\WordServiceInterface;
 use App\Events\DurationSaved;
+use App\Events\NewFilteredWordsStored;
 use App\Events\SentenceSaved;
 use App\Listeners\SaveFilteredWords;
 use App\Listeners\SaveSentences;
+use App\Listeners\UpdateDefiniton;
 use App\Services\CaptionService;
+use App\Services\DefinitionSelectorService;
 use App\Services\DefinitionsService;
 use App\Services\EnhancementService;
 use App\Services\FilteredWordService;
@@ -44,11 +50,14 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(FilteredWordServiceInterface::class, FilteredWordService::class);
         $this->app->bind(SentenceServiceInterface::class, SentenceService::class);
         $this->app->bind(WordServiceInterface::class, WordService::class);
+        $this->app->bind(DefinitionSelectorServiceInterface::class, DefinitionSelectorService::class);
+
         $this->app->bind(DefinitionsApiInterface::class, FreeDictionaryApi::class);
         $this->app->bind(SentencesApiInterface::class, FirstPartySentencingApi::class);
         $this->app->bind(SrtParserInterface::class, BenlippStrParser::class);
         $this->app->bind(WordFilterApiInterface::class, FirstPartyWordFilterApi::class);
         $this->app->bind(YoutubeCaptionsGrabberApiInterface::class, FirstPartyYoutubeCaptionsGrabberApi::class);
+        $this->app->bind(DefinitionSelectorApiInterface::class, FirstPartyDefinitionSelectorApi::class);
 
     }
 
@@ -56,6 +65,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Event::listen(DurationSaved::class, SaveSentences::class);
         Event::listen(SentenceSaved::class, SaveFilteredWords::class);
+        Event::listen(NewFilteredWordsStored::class, UpdateDefiniton::class);
         Model::shouldBeStrict();
     }
 }
