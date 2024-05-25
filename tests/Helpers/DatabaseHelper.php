2eabd0c775d1f5d8cdac555393b04e64dab0bceb
wimukthi <9wimu9@gmail.com>
2024-05-25 16:36:52 +0100

    Created DatabaseHelper.php

---
+++ tests/Helpers/DatabaseHelper.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Helpers;
+
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Support\Facades\Event;
+
+trait DatabaseHelper
+{
+    public function logQueriesToConsole(): void
+    {
+        Event::listen(QueryExecuted::class, static function ($query) {
+            error_log(str_replace('\"', '"', json_encode([
+                'query' => $query->sql,
+                'bindings' => $query->bindings,
+                'time' => $query->time.'ms',
+            ])));
+        });
+    }
+}
