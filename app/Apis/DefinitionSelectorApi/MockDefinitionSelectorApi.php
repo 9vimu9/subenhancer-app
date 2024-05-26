371f703800066bcbd4d415546df57a8e6a1ac6e8
wimukthi <9wimu9@gmail.com>
2024-05-26 03:32:12 +0100

    Created MockDefinitionSelectorApi.php

---
+++ app/Apis/DefinitionSelectorApi/MockDefinitionSelectorApi.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Apis\DefinitionSelectorApi;
+
+use App\Core\Contracts\Apis\DefinitionSelectorApiInterface;
+
+class MockDefinitionSelectorApi implements DefinitionSelectorApiInterface
+{
+    public function pickADefinitionBasedOnContext(string $context, array $definitionArray, string $word, int $orderInTheContext): string
+    {
+        return array_rand($definitionArray);
+    }
+}
