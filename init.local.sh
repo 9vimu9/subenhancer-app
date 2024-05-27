a91242b2242980d2d46e222740fdbbb65da46034
wimukthi <9wimu9@gmail.com>
2024-05-27 22:43:44 +0100

    improved init.local.sh

--- init.local.sh
+++ init.local.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
+
+source .env
+
 # copy pre-commit
 \cp -r git/hooks .git/
 chmod +x .git/hooks/pre-commit
 
+timestamp=$(date +%s)
+
+#dump before refresh
+mysqldump -h0.0.0.0 -P$FORWARD_DB_PORT -u root -p$DB_PASSWORD $DB_DATABASE corpuses > "${timestamp}-corpuses.sql"
+mysqldump -h0.0.0.0 -P$FORWARD_DB_PORT -u root -p$DB_PASSWORD $DB_DATABASE definitions > "${timestamp}-definitions.sql"
 
 ./vendor/bin/sail php artisan migrate:refresh
 
+#restock
+mysql -h0.0.0.0 -P$FORWARD_DB_PORT -u root -p$DB_PASSWORD $DB_DATABASE < "${timestamp}-corpuses.sql"
+mysql -h0.0.0.0 -P$FORWARD_DB_PORT -u root -p$DB_PASSWORD $DB_DATABASE < "${timestamp}-definitions.sql"
 # run essential seeds
 ./vendor/bin/sail php artisan db:seed
 
