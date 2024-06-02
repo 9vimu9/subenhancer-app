28cf92543e90f8ac03c8a1315a8f6a6722ad8704
wimukthi <9wimu9@gmail.com>
2024-06-03 00:20:14 +0100

    remove unwanted method in SentenceServiceInterface.php

--- app/Core/Contracts/Services/SentenceServiceInterface.php
+++ app/Core/Contracts/Services/SentenceServiceInterface.php
@@ -5,13 +5,9 @@ declare(strict_types=1);
 namespace App\Core\Contracts\Services;
 
 use App\DataObjects\Captions\Caption;
-use App\DataObjects\Sentences\Sentence;
 use App\DataObjects\Sentences\SentenceCollection;
-use Illuminate\Database\Eloquent\Model;
 
 interface SentenceServiceInterface
 {
     public function captionToSentences(Caption $caption): SentenceCollection;
-
-    public function storeSentence(int $durationId, Sentence $sentence): Model;
 }
