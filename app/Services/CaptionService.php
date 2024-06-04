58b690aad60c0ad793622a7b98574cc804c1113d
wimukthi <9wimu9@gmail.com>
2024-06-04 12:04:04 +0100

    refactor CaptionService.php

--- app/Services/CaptionService.php
+++ app/Services/CaptionService.php
@@ -8,6 +8,7 @@ use App\Core\Contracts\Services\CaptionServiceInterface;
 use App\Core\Contracts\Services\DefinitionSelectorServiceInterface;
 use App\Core\Contracts\Services\SentenceServiceInterface;
 use App\Core\Database\LastInsertedIdTrait;
+use App\DataObjects\Captions\Caption;
 use App\DataObjects\Captions\CaptionsCollection;
 use App\Dtos\CaptionwordDto;
 use App\Dtos\CaptionwordDtoCollection;
@@ -61,19 +62,47 @@ class CaptionService implements CaptionServiceInterface
         foreach ($captionsCollection as $caption) {
             $currentDurationId++;
             $durations->add(new DurationDto(id: $currentDurationId, startTime: $caption->getStartTime(), endTime: $caption->getEndTime(), sourceId: $sourceId));
-            foreach ($this->sentenceService->captionToSentences($caption) as $sentence) {
-                $currentSentenceId++;
-                $sentences->add(new SentenceDto(id: $currentSentenceId, order: $sentence->getOrder(), sentence: $sentence->getSentence(), durationId: $currentDurationId));
-
-                $filteredWordsInSentence = $this->getIncludedFilteredWordsInTheSentence($sentence->getSentence(), $filteredWordsWithIdsArray);
-                foreach ($filteredWordsInSentence as $order => $filteredWord) {
-                    $currentFilteredWordId++;
-                    $captionWords->add(new CaptionwordDto(id: $currentFilteredWordId, order: $order, sentenceId: $currentSentenceId, definitionId: $this->definitionSelectorService->findMostSuitableDefinitionId($sentence, $filteredWord, $order)));
-                }
-            }
+            $this->processSentences(
+                $caption,
+                $currentSentenceId,
+                $sentences,
+                $currentDurationId,
+                $filteredWordsWithIdsArray,
+                $currentFilteredWordId,
+                $captionWords);
         }
         Duration::query()->insertOrIgnore($durations->toArray());
         Sentence::query()->insertOrIgnore($sentences->toArray());
         Captionword::query()->insertOrIgnore($captionWords->toArray());
+    }
+
+    private function processSentences(
+        Caption $caption,
+        int &$currentSentenceId,
+        SentenceDtoCollection $sentences,
+        int $currentDurationId,
+        array $filteredWordsWithIdsArray,
+        int &$currentFilteredWordId,
+        CaptionwordDtoCollection $captionWords): void
+    {
+        foreach ($this->sentenceService->captionToSentences($caption) as $sentence) {
+            $currentSentenceId++;
+            $sentences->add(new SentenceDto(id: $currentSentenceId, order: $sentence->getOrder(), sentence: $sentence->getSentence(), durationId: $currentDurationId));
+            $this->processFilteredWords($sentence, $filteredWordsWithIdsArray, $currentFilteredWordId, $captionWords, $currentSentenceId);
+        }
+    }
+
+    private function processFilteredWords(
+        \App\DataObjects\Sentences\Sentence $sentence,
+        array $filteredWordsWithIdsArray,
+        int &$currentFilteredWordId,
+        CaptionwordDtoCollection $captionWords,
+        int $currentSentenceId): void
+    {
+        $filteredWordsInSentence = $this->getIncludedFilteredWordsInTheSentence($sentence->getSentence(), $filteredWordsWithIdsArray);
+        foreach ($filteredWordsInSentence as $order => $filteredWord) {
+            $currentFilteredWordId++;
+            $captionWords->add(new CaptionwordDto(id: $currentFilteredWordId, order: $order, sentenceId: $currentSentenceId, definitionId: $this->definitionSelectorService->findMostSuitableDefinitionId($sentence, $filteredWord, $order)));
+        }
     }
 }
