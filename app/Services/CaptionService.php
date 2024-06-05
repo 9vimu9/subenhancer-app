c7b4b3f07aad86d474d9477dd2a8f50463b67d9a
wimukthi <9wimu9@gmail.com>
2024-06-05 16:39:03 +0100

    fix recurrent primary keys Captionword DTO collections

--- app/Services/CaptionService.php
+++ app/Services/CaptionService.php
@@ -73,7 +73,7 @@ class CaptionService implements CaptionServiceInterface
         SentenceDtoCollection $sentences,
         int $currentDurationId,
         CorpusDtoCollection $filteredWordsCorpusDtoCollection,
-        int $currentFilteredWordId,
+        int &$currentFilteredWordId,
         CaptionwordDtoCollection $captionWords): void
     {
         foreach ($this->sentenceService->captionToSentences($caption) as $sentence) {
