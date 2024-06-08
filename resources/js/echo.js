7bd3003948da1c7590320d92c92433fea87b24d4
wimukthi <9wimu9@gmail.com>
2024-06-08 18:39:12 +0100

    set PUSHER_DEBUG env variable

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,7 +1,10 @@
 import Echo from 'laravel-echo';
 
 import Pusher from 'pusher-js';
+
 window.Pusher = Pusher;
+window.Pusher.logToConsole=import.meta.env.VITE_PUSHER_DEBUG?.toLowerCase?.() === 'true'
+
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
