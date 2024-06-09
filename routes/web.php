2241c9f5a8a283164f765f963aa1ba716b788a6e
wimukthi <9wimu9@gmail.com>
2024-06-09 02:26:47 +0100

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\EnhancementController;
 use App\Http\Controllers\EnhanceSubmissionController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -12,12 +16,11 @@ Route::get('/play', function () {
     return view('playground');
 })->name('playground');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'dashboard'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::post('/submissions', [EnhanceSubmissionController::class, 'submit'])->name('submitEnhancement');
+    Route::get('/enhancements/{enhancementId}', [EnhancementController::class, 'show'])->name('enhancement.create');
 });
 
 Route::middleware('auth')->group(function () {
